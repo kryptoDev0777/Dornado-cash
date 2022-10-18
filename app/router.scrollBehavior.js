06315a22a09e9cf3a7e8493a6a8529cee8c120a5
Jeremic.N <95298844+starman1121@users.noreply.github.com>
2022-10-19 01:30:44 +1000

    Update router.scrollBehavior.js

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,6 +1,6 @@
 const routerScrollBehavior = (to, from, savedPosition) => {
   if (to.name === 'governance-id') {
-    return { x: 0, y: 0 }
+    return { x: 0, y: 0 
   }
 
   return savedPosition || { x: 0, y: 0 }
